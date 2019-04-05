34eb5d9366a217fb69cc8b197f5b2fc1ed8b398b
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-05 16:15:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function lowerCaseDrivers(drivers){
 }
 
 function nameToAttributes(drivers){
-  drivers.map(d => {firstName: d.split(" ").first, lastName: d.split(" ").last})
+  drivers.map(d => {{firstName: d.split(" ").first, lastName: d.split(" ").last}})
 }
